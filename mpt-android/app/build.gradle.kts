780852107c9a269ae52cabf1d09d68ced589b674
edwinbulter <edwin.bulter@gmail.com>
2026-01-02 15:53:18 +0100

    [ECS-32] Fix versionCode extraction in deployment workflow and bump versionCode to 4
    - Change versionCode extraction from space-based to equals-based delimiter with whitespace trimming
    - Update grep command from `cut -d' ' -f3` to `cut -d'=' -f2 | tr -d ' '` for more reliable parsing
    - Increment versionCode from 3 to 4 in build.gradle.kts for new release version

--- mpt-android/app/build.gradle.kts
+++ mpt-android/app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         applicationId = "ebulter.multiply"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
