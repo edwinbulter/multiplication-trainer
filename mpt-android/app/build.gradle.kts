0f8c327a2070c51180ae468a52a2ffb7f7bbf146
edwinbulter <edwin.bulter@gmail.com>
2026-01-02 14:21:36 +0100

    [ECS-32] Bump Android app versionCode from 1 to 2
    - Increment versionCode to 2 in build.gradle.kts for new release version

--- mpt-android/app/build.gradle.kts
+++ mpt-android/app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         applicationId = "ebulter.multiply"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
