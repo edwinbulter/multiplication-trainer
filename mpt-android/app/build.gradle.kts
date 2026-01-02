ef84b6c323d771fe4c81122966b7ca922b9b6b83
edwinbulter <edwin.bulter@gmail.com>
2026-01-02 15:44:37 +0100

    [ECS-32] Fix release name format to use versionCode instead of duplicate versionName and bump versionCode to 3
    - Add version_code output to version extraction step in deployment workflow
    - Correct releaseName format from "Release {versionName}/{versionName}/{runNumber}" to "Release {versionName}/{versionCode}/{runNumber}"
    - Increment versionCode from 2 to 3 in build.gradle.kts for new release version

--- mpt-android/app/build.gradle.kts
+++ mpt-android/app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         applicationId = "ebulter.multiply"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
