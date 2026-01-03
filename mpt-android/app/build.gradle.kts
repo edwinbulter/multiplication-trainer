999065cff813a795658cba1e001947b3110090d8
edwinbulter <edwin.bulter@gmail.com>
2026-01-03 22:45:49 +0100

    [ECS-32] Increment version name from 1.0.2 to 1.0.3

--- mpt-android/app/build.gradle.kts
+++ mpt-android/app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 9
-        versionName = "1.0.2"
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
