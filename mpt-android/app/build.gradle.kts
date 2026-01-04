391812e0a32b9542afe921da8397ecd691be6d0c
GitHub Action <action@github.com>
2026-01-04 17:53:09 +0000

    🔄 Auto-increment version code to 11

--- mpt-android/app/build.gradle.kts
+++ mpt-android/app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         applicationId = "ebulter.multiply"
         minSdk = 24
         targetSdk = 36
-        versionCode = 10
+        versionCode = 11
         versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
